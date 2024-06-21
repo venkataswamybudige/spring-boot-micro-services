7b610c950d3e41ee40a29cb51f664bebeb9408c0
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2024-06-21 20:38:17 +0530

    Feign configuration

--- currency-conversion-service/src/main/java/com/venkat/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/venkat/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "current-exchange-service",url = "localhost:8000")
+//@FeignClient(name = "current-exchange-service",url = "localhost:8000")
+@FeignClient(name = "current-exchange-service")
+
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/current-exchange/from/{from}/to/{to}")
